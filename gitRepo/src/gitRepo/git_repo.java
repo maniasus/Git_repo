0fbb04272614a90b08ee397237e57ac2efd37633
maniasus <maniasus92@gmail.com>
2022-06-17 22:37:57 +0530

    upload the message

--- gitRepo/src/gitRepo/git_repo.java
+++ gitRepo/src/gitRepo/git_repo.java
@@ -5,6 +5,7 @@ public class git_repo {
 	public static void main(String[] args) {
 		System.out.println("hi git_repo");
 		System.out.println("hi git how r u");
+		System.out.println("hi git fine");
 
 	}
 
